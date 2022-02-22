3dcc2697ea77cacde3bbc37627c56892df19866b
NnelisS <78425965+NnelisS@users.noreply.github.com>
2022-02-22 16:23:25 +0100

    input manager both inventory

--- spaceship horror/Assets/Niels/Scripts/Interact.cs
+++ spaceship horror/Assets/Niels/Scripts/Interact.cs
@@ -9,17 +9,19 @@ public class Interact : MonoBehaviour
     private void OnMouseDown()
     {
         inventory.items.Add(this.gameObject);
-        inventory.objectives.RemoveAt(0);
-        Destroy(this.gameObject);
+        inventory.UpdateUI();
+        gameObject.SetActive(false);
     }
 
     public void OnMouseOver()
     {
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255);
         Debug.Log("Hovering");
     }
 
     private void OnMouseExit()
     {
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0);
         Debug.Log("NotHovering");
     }
 }
--- spaceship horror/Assets/Niels/Scripts/Inventory.cs
+++ spaceship horror/Assets/Niels/Scripts/Inventory.cs
@@ -7,8 +7,6 @@ public class Inventory : MonoBehaviour
     public List<GameObject> items = new List<GameObject>();
     public List<GameObject> objectives = new List<GameObject>();
 
-    public int currentCount;
-
     private void Start()
     {
         for (int i = 0; i < objectives.Count; i++)
@@ -18,9 +16,15 @@ public class Inventory : MonoBehaviour
     }
 
     private void Update()
-    {
-        currentCount = items.Count;
-        
+    {        
         Debug.Log(items.Count);
+    }
+
+    public void UpdateUI()
+    {
+        for (int i = objectives.Count - items.Count; i < objectives.Count; i++)
+        {
+            objectives[i].SetActive(false);
+        }
     }
 }
